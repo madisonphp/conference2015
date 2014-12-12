05c4a4a7bd4a32bb6829bcabc9849676603b5f31
Andrew Shell <andrew@andrewshell.org>
2014-12-12 09:55:15 -0600

    Fleshing out the layout, navbar and header in general

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
-phpinfo();
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/views',
+));
+
+$app['nav'] = array(
+    'Home'  => '/',
+);
+
+$app->get('/', function() use($app) {
+    return $app['twig']->render('layout.twig.html', array(
+        'nav' => $app['nav'],
+        'active' => 'Home',
+    ));
+});
+
+$app->run();
