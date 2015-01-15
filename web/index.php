66731b3cfb7b57003d11c0b78a6d5df6a07b299b
Andrew Shell <andrew@andrewshell.org>
2015-01-15 12:17:29 -0600

    Structuring better for different hosting

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__ . '/views',
+    'twig.path' => __DIR__ . '/../views',
 ));
 
 $app['nav'] = array(
