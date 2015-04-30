b3973b3c6bd2ce1268cdfbf8389a3cab5602a20e
Andrew Shell <andrew@andrewshell.org>
2015-04-30 12:06:23 -0500

    Setting up automatic updates

---
+++ web/gitpull.php
@@ -0,0 +1,7 @@
+<?php
+header("Content-type: text/plain"); // be explicit to avoid accidental XSS
+
+$gitpath = '/usr/bin/git';
+chdir(__DIR__ . '/../'); // rarely actually an acceptable thing to do
+system("/usr/bin/env -i {$gitpath} pull 2>&1"); // main repo (current branch)
+system("/usr/bin/env -i COMPOSER_HOME=./.composer php composer.phar install 2>&1"); // composer
